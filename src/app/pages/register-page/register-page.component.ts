887048ba65ea1b1a67bb24eab497a933611ad516
Mauricio Reyes <mreyesc@gmail.com>
2023-06-12 19:13:55 -0500

    issues solved on register page

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -12,7 +12,7 @@ export class RegisterPageComponent implements OnInit {
   userEmail!: string;
   userPassword!: string;
   userPasswordConfirm!: string;
-  passwordConfirmed!:string;
+  passwordConfirmed!: string;
   userBirthday!: string;
   userCity!: string;
   userMotorcycle!: string;
@@ -22,45 +22,73 @@ export class RegisterPageComponent implements OnInit {
   userMotorcycleYear!: string;
   errorMessage: string = '';
 
-
   constructor(private userService: UserservicesService) {}
 
   ngOnInit() {
+    this.userMotorcycleCheck();
     this.userService.creationError$.subscribe((errorMessage: string) => {
       this.errorMessage = errorMessage;
     });
   }
 
-  motorcycleChequed(): void {
-    const motorcycleChecked = document.getElementById('userMotorcycleYes') as HTMLInputElement;
-    const motorcycleBrand = document.getElementById('userMotorcycleBrand') as HTMLInputElement;
-    const motorcycleModel = document.getElementById('userMotorcycleModel') as HTMLInputElement;
-    const motorcycleType = document.getElementById('userMotorcycleType') as HTMLInputElement;
-    const motorcycleYear = document.getElementById('userMotorcycleYear') as HTMLInputElement;
+  userMotorcycleCheck(): void {
+    const motorcycleChecked1 = document.getElementById(
+      'flexRadioDefault1'
+    ) as HTMLInputElement;
+    const motorcycleChecked2 = document.getElementById(
+      'flexRadioDefault2'
+    ) as HTMLInputElement;
+    const motorcycleBrand = document.getElementById(
+      'userMotorcycleBrand'
+    ) as HTMLInputElement;
+    const motorcycleModel = document.getElementById(
+      'userMotorcycleModel'
+    ) as HTMLInputElement;
+    const motorcycleType = document.getElementById(
+      'userMotorcycleType'
+    ) as HTMLInputElement;
+    const motorcycleYear = document.getElementById(
+      'userMotorcycleYear'
+    ) as HTMLInputElement;
 
-    if(motorcycleChecked.value === "yes") {
-        motorcycleBrand.disabled = false;
-        motorcycleModel.disabled = false;
-        motorcycleType.disabled = false;
-        motorcycleYear.disabled = false;
-      } else {
-        motorcycleBrand.disabled = true;
-        motorcycleModel.disabled = true;
-        motorcycleType.disabled = true;
-        motorcycleYear.disabled = true;
-      }
+    if (motorcycleChecked1.checked) {
+      this.userMotorcycle = 'true';
+      console.log(this.userMotorcycle);
+      motorcycleBrand.disabled = false;
+      motorcycleModel.disabled = false;
+      motorcycleType.disabled = false;
+      motorcycleYear.disabled = false;
+    } else if (motorcycleChecked2.checked) {
+      this.userMotorcycle = 'false';
+      console.log(this.userMotorcycle);
+      motorcycleBrand.disabled = true;
+      motorcycleModel.disabled = true;
+      motorcycleType.disabled = true;
+      motorcycleYear.disabled = true;
+    }
   }
 
-  createUser():void {
-    if(this.userPassword !== this.userPasswordConfirm) {
-      this.errorMessage = "Las contraseñas ingresadas no coinciden"
+  createUser(): void {
+    if (this.userPassword !== this.userPasswordConfirm) {
+      this.errorMessage = 'Las contraseñas ingresadas no coinciden';
     } else {
       this.passwordConfirmed = this.userPasswordConfirm;
-      this.userService.create(this.userFirstName, this.userLastName, this.userEmail, this.passwordConfirmed, this.userBirthday, this.userCity, this.userMotorcycle, this.userMotorcycleBrand, this.userMotorcycleModel, this.userMotorcycleType, this.userMotorcycleYear);
+      this.userService.create(
+        this.userFirstName,
+        this.userLastName,
+        this.userEmail,
+        this.passwordConfirmed,
+        this.userBirthday,
+        this.userCity,
+        this.userMotorcycle,
+        this.userMotorcycleBrand,
+        this.userMotorcycleModel,
+        this.userMotorcycleType,
+        this.userMotorcycleYear
+      );
     }
-
   }
-  
+
   clearErrorMessage(): void {
     this.errorMessage = '';
   }
